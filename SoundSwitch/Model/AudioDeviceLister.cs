3106c9e04f757aa9a0306a6895b90d19009db45f
Antoine Aflalo <antoine@aaflalo.me>
2015-12-22 07:02:55 -0500

    Add High level caching of Audio Device Reduce the work of the GC

--- SoundSwitch/Model/AudioDeviceLister.cs
+++ SoundSwitch/Model/AudioDeviceLister.cs
@@ -12,7 +12,10 @@
 * GNU General Public License for more details.
 ********************************************************************/
 
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading;
 using AudioEndPoint;
 using AudioEndPointControllerWrapper;
 using SoundSwitch.Framework;
@@ -22,10 +25,81 @@ namespace SoundSwitch.Model
     public class AudioDeviceLister : IAudioDeviceLister
     {
         private readonly DeviceState _state;
+        private readonly HashSet<IAudioDevice> _recording = new HashSet<IAudioDevice>();
+        private readonly HashSet<IAudioDevice> _playback = new HashSet<IAudioDevice>();
+        private bool _needUpdate = true;
+        private readonly ReaderWriterLockSlim _cacheLock = new ReaderWriterLockSlim();
 
         public AudioDeviceLister(DeviceState state)
         {
             _state = state;
+            AudioController.DeviceAdded += AudioControllerOnDeviceAdded;
+            AudioController.DeviceRemoved += AudioControllerOnDeviceRemoved;
+            AudioController.DeviceStateChanged += AudioControllerOnDeviceStateChanged;
+        }
+
+        private void AudioControllerOnDeviceStateChanged(object sender, DeviceStateChangedEvent deviceStateChangedEvent)
+        {
+            _cacheLock.EnterWriteLock();
+            try
+            {
+                using (AppLogger.Log.InfoCall())
+                {
+                    if (deviceStateChangedEvent.newState == _state)
+                    {
+                        if (deviceStateChangedEvent.device.Type == AudioDeviceType.Playback)
+                        {
+                            _playback.Add(deviceStateChangedEvent.device);
+                            AppLogger.Log.Info("Add new playback: ",deviceStateChangedEvent.device);
+                        }
+                        else
+                        {
+                            _recording.Add(deviceStateChangedEvent.device);
+                            AppLogger.Log.Info("Add new recording: ", deviceStateChangedEvent.device);
+                        }
+                    }
+                    else if (deviceStateChangedEvent.previousState == _state)
+                    {
+                        if (deviceStateChangedEvent.device.Type == AudioDeviceType.Playback)
+                        {
+                            _playback.Remove(deviceStateChangedEvent.device);
+                            AppLogger.Log.Info("Remove playback: ", deviceStateChangedEvent.device);
+                        }
+                        else
+                        {
+                            _recording.Remove(deviceStateChangedEvent.device);
+                            AppLogger.Log.Info("Remove Recording: ", deviceStateChangedEvent.device);
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                _cacheLock.ExitWriteLock();
+            }
+        }
+
+        private void AudioControllerOnDeviceRemoved(object sender, DeviceRemovedEvent deviceRemovedEvent)
+        {
+            SetNeedUpdate();
+        }
+
+        private void AudioControllerOnDeviceAdded(object sender, DeviceAddedEvent deviceAddedEvent)
+        {
+            SetNeedUpdate();
+        }
+
+        private void SetNeedUpdate()
+        {
+            _cacheLock.EnterWriteLock();
+            try
+            {
+                _needUpdate = true;
+            }
+            finally
+            {
+                _cacheLock.ExitWriteLock();
+            }
         }
 
         /// <summary>
@@ -34,16 +108,56 @@ namespace SoundSwitch.Model
         /// <returns></returns>
         public ICollection<IAudioDevice> GetPlaybackDevices()
         {
+            _cacheLock.EnterUpgradeableReadLock();
             try
             {
-                return AudioController.GetPlaybackDevices(_state);
+                if (_needUpdate)
+                {
+                    Refresh();
+                }
+                return _playback;
             }
-            catch (DefSoundException e)
+            finally
             {
-                AppLogger.Log.Error("Problem getting Playback Devices ", e);
-                return new List<IAudioDevice>();
+                AppLogger.Log.Debug("Get Playback Devices");
+                _cacheLock.ExitUpgradeableReadLock();
             }
-            
+
+
+        }
+
+        private void Refresh()
+        {
+            _cacheLock.EnterWriteLock();
+            try
+            {
+                _playback.Clear();
+
+                try
+                {
+                    _playback.UnionWith(AudioController.GetPlaybackDevices(_state));
+                }
+                catch (DefSoundException e)
+                {
+                    AppLogger.Log.Error("Problem getting Playback Devices ", e);
+                }
+
+
+                _recording.Clear();
+                try
+                {
+                    _recording.UnionWith(AudioController.GetRecordingDevices(_state));
+                }
+                catch (DefSoundException e)
+                {
+                    AppLogger.Log.Error("Problem getting Recording Devices ", e);
+                }
+                _needUpdate = false;
+            }
+            finally
+            {
+                _cacheLock.ExitWriteLock();
+            }
         }
 
         /// <summary>
@@ -52,16 +166,20 @@ namespace SoundSwitch.Model
         /// <returns></returns>
         public ICollection<IAudioDevice> GetRecordingDevices()
         {
+            _cacheLock.EnterUpgradeableReadLock();
             try
             {
-                return AudioController.GetRecordingDevices(_state);
+                if (_needUpdate)
+                {
+                    Refresh();
+                }
+                return _recording;
             }
-            catch (DefSoundException e)
+            finally
             {
-                AppLogger.Log.Error("Problem getting Recording Devices ", e);
-                return new List<IAudioDevice>();
+                AppLogger.Log.Debug("Get Recording Devices");
+                _cacheLock.ExitUpgradeableReadLock();
             }
-
         }
     }
 }
