938e2659f0d69b2755e419abbea5af4164f1ba5d
Antoine Aflalo <antoine@aaflalo.me>
2016-01-04 19:12:24 -0500

    Add possibility to get the state of the build

---
+++ SoundSwitch/Util/AssemblyUtils.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace SoundSwitch.Util
+{
+    public static class AssemblyUtils
+    {
+        public enum ReleaseState
+        {
+            Stable,
+            Beta
+        }
+        /// <summary>
+        /// Get Current Assembly configuration
+        /// </summary>
+        /// <returns></returns>
+        public static AssemblyConfigurationAttribute GetAssemblyConfigurationAttribute()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var attributes = assembly.GetCustomAttributes(true);
+            var config = attributes.OfType<AssemblyConfigurationAttribute>().FirstOrDefault();
+            return config;
+        }
+        /// <summary>
+        /// Get the current state of the application
+        /// </summary>
+        /// <returns></returns>
+        public static ReleaseState GetReleaseState()
+        {
+            return GetAssemblyConfigurationAttribute().Configuration == "Beta" ? ReleaseState.Beta : ReleaseState.Stable;
+        }
+    }
+}
