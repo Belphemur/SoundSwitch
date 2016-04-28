6cf4255f7b0a7b3820820b8aa723b824ec9de75e
Antoine Aflalo <antoine@aaflalo.me>
2016-04-28 17:29:31 -0400

    Cleanup file and documentation

--- SoundSwitch/Framework/NotificationManager/NotificationFactory.cs
+++ SoundSwitch/Framework/NotificationManager/NotificationFactory.cs
@@ -1,16 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Windows.Forms;
-using SoundSwitch.Framework.Audio;
+
 using SoundSwitch.Framework.NotificationManager.Notification;
-using SoundSwitch.Model;
-using SoundSwitch.Properties;
 
 namespace SoundSwitch.Framework.NotificationManager
 {
     public class NotificationFactory
     {
-        public static readonly List<INotification> AllNotifications = new List<INotification>()
+        public static readonly List<INotification> AllNotifications = new List<INotification>
         {
             new NotificationWindows(),
             new NotificationSound(),
@@ -22,7 +19,6 @@ namespace SoundSwitch.Framework.NotificationManager
         ///     Create a notification object linked to the enum value
         /// </summary>
         /// <param name="eEnum"></param>
-        /// <exception cref="CachedSoundFileNotExistsException">For a CustomNotification if the CustomSound file is not present.</exception>
         /// <returns></returns>
         public static INotification CreateNotification(NotificationTypeEnum eEnum)
         {
