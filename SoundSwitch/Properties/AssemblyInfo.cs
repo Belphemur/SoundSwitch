61b2a460a53c21e638ff4fff5cc5281001a4da01
Antoine Aflalo <antoine@aaflalo.me>
2016-05-15 12:40:16 -0400

    Bump Version

--- SoundSwitch/Properties/AssemblyInfo.cs
+++ SoundSwitch/Properties/AssemblyInfo.cs
@@ -35,4 +35,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.9.9.*")]
+[assembly: AssemblyVersion("3.10.0.*")]
