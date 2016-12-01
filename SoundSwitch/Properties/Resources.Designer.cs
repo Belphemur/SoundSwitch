ba4b9567e54cf36fed947b1f18f2c935681e79bd
Antoine Aflalo <antoine@aaflalo.me>
2016-11-30 20:55:57 -0500

    Add donate icon
    See #127

--- SoundSwitch/Properties/Resources.Designer.cs
+++ SoundSwitch/Properties/Resources.Designer.cs
@@ -91,6 +91,16 @@ namespace SoundSwitch.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon donate {
+            get {
+                object obj = ResourceManager.GetObject("donate", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap exit {
