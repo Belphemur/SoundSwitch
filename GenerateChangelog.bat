b73ef7f9a5cd28a3a2e0d8999fff52d7b5a15c4f
Antoine Aflalo <antoine@aaflalo.me>
2017-04-16 11:45:12 -0400

    Add changelog script

---
+++ GenerateChangelog.bat
@@ -0,0 +1,23 @@
+SETLOCAL
+SET "FILE_DIR=%~dp0"
+
+IF NOT EXIST "%FILE_DIR%..\github-key.txt" (
+  ECHO %~nx0: %FILE_DIR%..\github-key.txt is not present!
+  SET SIGN_ERROR=True
+  GOTO END
+)
+
+SET KEY=
+SET /P KEY=<"%FILE_DIR%..\github-key.txt"
+
+github_changelog_generator -t %KEY%
+
+
+:END
+IF /I "%SIGN_ERROR%" == "True" (
+  IF "%~1" == "" PAUSE
+  ENDLOCAL
+  EXIT /B 1
+)
+ENDLOCAL
+EXIT /B
