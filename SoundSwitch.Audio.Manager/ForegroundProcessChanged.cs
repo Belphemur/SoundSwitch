a764c1c75ef64ff88ce9808e6d38861399bb45e0
Antoine Aflalo <antoine@aaflalo.me>
2019-12-18 09:20:39 -0500

    First version of getting the foreground process changed

---
+++ SoundSwitch.Audio.Manager/ForegroundProcessChanged.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using SoundSwitch.Audio.Manager.Interop.Com.User;
+
+namespace SoundSwitch.Audio.Manager
+{
+    public class ForegroundProcessChanged
+    {
+        public class ForegroundProcessChangedEvent : EventArgs
+        {
+            /// <summary>
+            /// ID of the process that is now active
+            /// </summary>
+            public uint ProcessId { get; }
+
+            /// <summary>
+            /// Name of the process that is active
+            /// </summary>
+            public string ProcessName { get; }
+
+
+            public ForegroundProcessChangedEvent(uint processId, string processName)
+            {
+                ProcessId = processId;
+                ProcessName = processName;
+            }
+        }
+
+        public event EventHandler<ForegroundProcessChangedEvent> Event;
+
+        private readonly User32.NativeMethods.WinEventDelegate _winEventDelegate;
+
+
+        public ForegroundProcessChanged()
+        {
+            _winEventDelegate = (hook, type, hwnd, idObject, child, thread, time) =>
+            {
+                User32.NativeMethods.GetWindowThreadProcessId(hwnd, out var processId);
+                var process = Process.GetProcessById((int) processId);
+                var processName = process.MainModule?.FileName;
+                Event?.Invoke(this, new ForegroundProcessChangedEvent(processId, processName));
+            };
+
+            User32.NativeMethods.SetWinEventHook(User32.NativeMethods.EVENT_SYSTEM_FOREGROUND,
+                User32.NativeMethods.EVENT_SYSTEM_FOREGROUND,
+                IntPtr.Zero, _winEventDelegate,
+                0,
+                0,
+                User32.NativeMethods.WINEVENT_OUTOFCONTEXT);
+        }
+    }
+}
