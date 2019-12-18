a4e8aaf6e6912a173c82c3efc6102830023c16d9
Antoine Aflalo <antoine@aaflalo.me>
2019-12-18 12:27:21 -0500

    Add basic profile

---
+++ SoundSwitch/Framework/Profile/ProfileSettings.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using SoundSwitch.Framework.Audio.Device;
+
+namespace SoundSwitch.Framework.Profile
+{
+#nullable enable
+    public class ProfileSettings : IEquatable<ProfileSettings>
+    {
+        public string ProfileName { get; set; }
+        public string? ApplicationPath { get; set; }
+        public HotKeys? HotKeys { get; set; }
+        public DeviceInfo? Playback { get; set; }
+        public DeviceInfo? Recording { get; set; }
+
+        public bool Equals(ProfileSettings? other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return ProfileName == other.ProfileName;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((ProfileSettings) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return ProfileName.GetHashCode();
+        }
+
+        public static bool operator ==(ProfileSettings? left, ProfileSettings? right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(ProfileSettings? left, ProfileSettings? right)
+        {
+            return !Equals(left, right);
+        }
+    }
+}
