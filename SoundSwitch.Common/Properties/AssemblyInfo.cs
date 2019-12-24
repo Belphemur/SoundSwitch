590bb8f70d091001c319b390ec777f5a976a2b19
Antoine Aflalo <antoine@aaflalo.me>
2019-12-24 12:41:50 +0200

    Fix copyright

--- SoundSwitch.Common/Properties/AssemblyInfo.cs
+++ SoundSwitch.Common/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SoundSwitch.Common")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
