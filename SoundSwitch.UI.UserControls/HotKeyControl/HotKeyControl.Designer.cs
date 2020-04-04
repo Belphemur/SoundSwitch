a02e4cd83032623f67b34dac02eadb91c457babc
Antoine Aflalo <soundswitch+github@aaflalo.me>
2020-04-03 21:10:43 -0400

    Add new HotKey control
    Better than the one I wrote

---
+++ SoundSwitch.UI.UserControls/HotKeyControl/HotKeyControl.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace SoundSwitch.UI.UserControls.HotKeyControl
+{
+    public partial class HotKeyControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components      = new System.ComponentModel.Container();
+            this.TextBox         = new System.Windows.Forms.TextBox();
+            this.ToolTipProvider = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // TextBox
+            // 
+            this.TextBox.Dock             =  System.Windows.Forms.DockStyle.Fill;
+            this.TextBox.Location         =  new System.Drawing.Point(0, 0);
+            this.TextBox.Name             =  "TextBox";
+            this.TextBox.ShortcutsEnabled =  false;
+            this.TextBox.Size             =  new System.Drawing.Size(255, 23);
+            this.TextBox.TabIndex         =  0;
+            this.TextBox.KeyDown          += new System.Windows.Forms.KeyEventHandler(this.TextBox_KeyDown);
+            this.TextBox.KeyUp            += new System.Windows.Forms.KeyEventHandler(this.TextBox_KeyUp);
+            this.TextBox.Leave            += new System.EventHandler(this.TextBox_Leave);
+            // 
+            // HotKeyControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode       = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.TextBox);
+            this.Font   = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name   = "HotKeyControl";
+            this.Size   = new System.Drawing.Size(255, 26);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBox;
+        private System.Windows.Forms.ToolTip ToolTipProvider;
+    }
+}
---
+++ SoundSwitch.UI.UserControls/HotKeyControl/HotKeyIsSetEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+using SoundSwitch.Common.WinApi.Keyboard;
+
+namespace SoundSwitch.UI.UserControls.HotKeyControl
+{
+    public delegate void HotKeyIsSetEventHandler(object sender, HotKeyIsSetEventArgs e);
+
+    public class HotKeyIsSetEventArgs : EventArgs
+    {
+        public HotKey HotKey { get;  }
+
+        public bool Cancel { get; set; }
+
+        public string CancelReason { get; set; }
+
+        public string Shortcut => HotKeyShared.CombineShortcut(HotKey.Modifier, HotKey.Keys);
+
+        public HotKeyIsSetEventArgs(HotKey hotKey)
+        {
+            HotKey = hotKey;
+        }
+    }
+}
