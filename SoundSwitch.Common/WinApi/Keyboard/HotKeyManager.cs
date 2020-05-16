bce98db5b2e4f1ba6d95ddf1ccadbf2adb57a2b8
Antoine Aflalo <soundswitch+github@aaflalo.me>
2020-05-16 14:47:16 -0400

    Add pressed hotkey to the action

--- SoundSwitch.Common/WinApi/Keyboard/HotKeyManager.cs
+++ SoundSwitch.Common/WinApi/Keyboard/HotKeyManager.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using Resolve.HotKeys;
 
 namespace SoundSwitch.Common.WinApi.Keyboard
 {
     public class HotKeyManager : IDisposable
     {
+        public static HotKeyManager Instance { get; } = new HotKeyManager();
         private readonly Dictionary<HotKey, Resolve.HotKeys.HotKey> _hotKeys = new Dictionary<HotKey, Resolve.HotKeys.HotKey>();
 
         /// <summary>
@@ -15,7 +17,7 @@ namespace SoundSwitch.Common.WinApi.Keyboard
         /// <param name="hotKey"></param>
         /// <param name="action"></param>
         /// <returns></returns>
-        public bool Register(HotKey hotKey, Action action)
+        public bool Register(HotKey hotKey, Action<HotKey> action)
         {
             if (_hotKeys.ContainsKey(hotKey))
                 return false;
@@ -24,10 +26,11 @@ namespace SoundSwitch.Common.WinApi.Keyboard
             try
             {
                 hotKeyHandle.Register();
-                hotKeyHandle.Pressed += (sender, args) => action();
+                hotKeyHandle.Pressed += (sender, args) => action(hotKey);
             }
-            catch (Win32Exception)
+            catch (Win32Exception e)
             {
+                Trace.WriteLine("Can't register hotkey", e.Message);
                 return false;
             }
 
@@ -48,8 +51,9 @@ namespace SoundSwitch.Common.WinApi.Keyboard
             {
                 hotKeyHandle.Dispose();
             }
-            catch (Win32Exception)
+            catch (Win32Exception e)
             {
+                Trace.WriteLine("Can't unregister hotkey", e.Message);
                 return false;
             }
 
