b26349f6ff4b262c8d17e2024414710a5ae935c9
Antoine Aflalo <soundswitch+github@aaflalo.me>
2020-06-10 18:48:52 -0400

    Add Community to menu
    Fixes #472

--- SoundSwitch/Properties/Resources.Designer.cs
+++ SoundSwitch/Properties/Resources.Designer.cs
@@ -81,6 +81,16 @@ namespace SoundSwitch.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon DiscordIcon {
+            get {
+                object obj = ResourceManager.GetObject("DiscordIcon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap donate {
