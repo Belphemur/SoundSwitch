abe98a8f7f6d45fffc77be65884f87f37a3ee684
Antoine Aflalo <soundswitch+github@aaflalo.me>
2020-08-19 15:30:58 -0400

    Add new trigger startup
    See #489

--- SoundSwitch/Framework/Profile/Trigger/TriggerFactory.cs
+++ SoundSwitch/Framework/Profile/Trigger/TriggerFactory.cs
@@ -10,7 +10,8 @@ namespace SoundSwitch.Framework.Profile.Trigger
             HotKey,
             Window,
             Process,
-            Steam
+            Steam,
+            Startup
         }
 
         private static readonly IEnumImplList<Enum, ITriggerDefinition> Impl =
@@ -32,9 +33,14 @@ namespace SoundSwitch.Framework.Profile.Trigger
         /// <summary>
         /// Maximum number of occurence of this trigger in a profile
         /// </summary>
-        public int MaxOccurence { get; set; }
+        public int MaxOccurence { get; }
 
-        public string Description { get; set; }
+        /// <summary>
+        /// Maximum number of occurence of this trigger globally
+        /// </summary>
+        public int MaxGlobalOccurence { get; }
+
+        public string Description { get; }
     }
 
     public abstract class BaseTrigger : ITriggerDefinition
@@ -44,40 +50,53 @@ namespace SoundSwitch.Framework.Profile.Trigger
             return Label;
         }
 
-        public virtual TriggerFactory.Enum TypeEnum { get; }
-        public virtual string Label { get; }
-        public virtual int MaxOccurence { get; set; } = -1;
-        public abstract string Description { get; set; }
+        public virtual  TriggerFactory.Enum TypeEnum           { get; }
+        public virtual  string              Label              { get; }
+        public virtual  int                 MaxOccurence       { get; } = -1;
+        public virtual  int                 MaxGlobalOccurence { get; } = -1;
+        public abstract string              Description        { get; }
     }
 
     public class HotKeyTrigger : BaseTrigger
     {
-        public override TriggerFactory.Enum TypeEnum { get; } = TriggerFactory.Enum.HotKey;
-        public override string Label => SettingsStrings.hotkeys;
-        public override string Description { get; set; } = SettingsStrings.profile_trigger_hotkey_desc;
-        public override int MaxOccurence { get; set; } = 1;
+        public override TriggerFactory.Enum TypeEnum     { get; } = TriggerFactory.Enum.HotKey;
+        public override string              Label        => SettingsStrings.hotkeys;
+        public override string              Description  { get; } = SettingsStrings.profile_trigger_hotkey_desc;
+        public override int                 MaxOccurence { get; } = 1;
     }
 
     public class WindowTrigger : BaseTrigger
     {
-        public override TriggerFactory.Enum TypeEnum { get; } = TriggerFactory.Enum.Window;
-        public override string Label => SettingsStrings.profile_trigger_window;
-        public override string Description { get; set; } = SettingsStrings.profile_trigger_window_desc;
+        public override TriggerFactory.Enum TypeEnum    { get; } = TriggerFactory.Enum.Window;
+        public override string              Label       => SettingsStrings.profile_trigger_window;
+        public override string              Description { get; } = SettingsStrings.profile_trigger_window_desc;
     }
 
     public class ProcessTrigger : BaseTrigger
     {
-        public override TriggerFactory.Enum TypeEnum { get; } = TriggerFactory.Enum.Process;
-        public override string Label => SettingsStrings.profile_trigger_process;
-        public override string Description { get; set; } = SettingsStrings.profile_trigger_process_desc;
+        public override TriggerFactory.Enum TypeEnum    { get; } = TriggerFactory.Enum.Process;
+        public override string              Label       => SettingsStrings.profile_trigger_process;
+        public override string              Description { get; } = SettingsStrings.profile_trigger_process_desc;
     }
 
     public class SteamBigPictureTrigger : BaseTrigger
     {
         public override TriggerFactory.Enum TypeEnum { get; } = TriggerFactory.Enum.Steam;
-        public override string Label => SettingsStrings.profile_trigger_steam;
+        public override string              Label    => SettingsStrings.profile_trigger_steam;
 
-        public override int MaxOccurence { get; set; } = 1;
-        public override string Description { get; set; } = SettingsStrings.profile_trigger_steam_desc;
+        public override int    MaxOccurence       { get; } = 1;
+        public override int    MaxGlobalOccurence { get; } = 1;
+        public override string Description        { get; } = SettingsStrings.profile_trigger_steam_desc;
+    }
+
+    public class Startup : BaseTrigger
+    {
+        public override TriggerFactory.Enum TypeEnum { get; } = TriggerFactory.Enum.Startup;
+        public override string              Label    => SettingsStrings.profile_trigger_startup;
+
+        public override int MaxOccurence       { get; } = 1;
+        public override int MaxGlobalOccurence { get; } = 1;
+
+        public override string Description { get; } = SettingsStrings.profile_trigger_startup_desc;
     }
 }
