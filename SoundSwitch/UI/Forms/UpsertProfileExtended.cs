72e0076bfa0e553bc25dde767c4e3b3fe73eac26
Antoine Aflalo <soundswitch+github@aaflalo.me>
2020-11-07 16:23:01 -0500

    Don't hide the checkbox
    It's already disabled when it needs to

--- SoundSwitch/UI/Forms/UpsertProfileExtended.cs
+++ SoundSwitch/UI/Forms/UpsertProfileExtended.cs
@@ -184,7 +184,7 @@ namespace SoundSwitch.UI.Forms
 
             restoreDevicesCheckBox.DataBindings.Clear();
 
-            if (triggerDefinition.CanRestoreDevices && _profile.AlsoSwitchDefaultDevice)
+            if (triggerDefinition.CanRestoreDevices)
             {
                 restoreDevicesCheckBox.DataBindings.Add(nameof(CheckBox.Checked), trigger, nameof(Trigger.ShouldRestoreDevices), true, DataSourceUpdateMode.OnPropertyChanged);
                 restoreDevicesCheckBox.Show();
