5de07975ae555b42c6718148ab51b445f434963f
Antoine Aflalo <soundswitch+github@aaflalo.me>
2020-11-21 14:30:07 -0500

    Bump version

--- SoundSwitch/Properties/AssemblyInfo.cs
+++ SoundSwitch/Properties/AssemblyInfo.cs
@@ -35,4 +35,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("5.5.5.*")]
+[assembly: AssemblyVersion("5.6.0.*")]
