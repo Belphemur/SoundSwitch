d45f8e2aa48f9f8242ea50bb190cf9aaacfb4966
Antoine Aflalo <soundswitch+github@aaflalo.me>
2021-01-09 16:37:43 -0500

    Update copyright

--- SoundSwitch.Audio.Manager/Properties/AssemblyInfo.cs
+++ SoundSwitch.Audio.Manager/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SoundSwitch")]
 [assembly: AssemblyProduct("SoundSwitch.Audio.Manager")]
-[assembly: AssemblyCopyright("Copyright © 2020")]
+[assembly: AssemblyCopyright("Copyright © 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- SoundSwitch.Common/Properties/AssemblyInfo.cs
+++ SoundSwitch.Common/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -10,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SoundSwitch.Common")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
