49cee296f440a5fbc068a2e15503e9d1d4dcb9a9
Antoine Aflalo <soundswitch+github@aaflalo.me>
2021-04-17 13:11:47 -0400

    refactor(Profile): Add new icon for profile

--- SoundSwitch/Properties/Resources.Designer.cs
+++ SoundSwitch/Properties/Resources.Designer.cs
@@ -210,6 +210,16 @@ namespace SoundSwitch.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap profile_menu_icon {
+            get {
+                object obj = ResourceManager.GetObject("profile-menu-icon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
         internal static System.Drawing.Icon program {
