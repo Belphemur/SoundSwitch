8cda3956ce0e9e802fa4f0348bd36f42712b75b1
Antoine Aflalo <soundswitch+github@aaflalo.me>
2021-05-18 11:46:51 -0400

    ci(Beta): set proper versionning in notification

--- devVersion.ps1
+++ devVersion.ps1
@@ -1,9 +1,14 @@
 $currentVersion = [Version]$(git describe --tags --abbrev=0).substring(1)
 $currentBuild = $Env:GITHUB_RUN_ID
-echo "BUILD : $currentBuild";
+$scriptPath = $MyInvocation.MyCommand.Path
+$scriptFolder = Split-Path $scriptPath -Parent
+
 $newBuild = [int]$currentBuild.substring(0,5)
 $newRevision = [int]$currentBuild.substring(5)
 
 $newVersion = [Version]::new($currentVersion.Major, $currentVersion.Minor, $newBuild, $newRevision)
 
+Set-Content -Path "$scriptFolder\version" -Value $newVersion.toString()
+
 echo $newVersion.toString()
+
