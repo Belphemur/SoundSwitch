8ea0ae69aa2d4acabe5b1344ad4c2d364524d57c
Antoine Aflalo <soundswitch+github@aaflalo.me>
2021-08-23 14:39:47 -0400

    refactor(AudioDeviceBox): Add component

---
+++ SoundSwitch/UI/Forms/Components/AudioDeviceBox.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+using SoundSwitch.Common.Framework.Audio.Device;
+
+namespace SoundSwitch.UI.Forms.Components
+{
+    public partial class AudioDeviceBox : UserControl
+    {
+        public AudioDeviceBox(DeviceFullInfo device)
+        {
+            iconBox.DataBindings.Add(nameof(PictureBox.Image), device, nameof(device.LargeIcon), false, DataSourceUpdateMode.OnPropertyChanged);
+            deviceName.DataBindings.Add(nameof(Label.Text), device, nameof(device.NameClean), false, DataSourceUpdateMode.OnPropertyChanged);
+        }
+    }
+}
