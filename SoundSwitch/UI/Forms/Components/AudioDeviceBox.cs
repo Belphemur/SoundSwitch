7ba1d18a3fb9db2cfcd00214daae81920af45b7c
Antoine Aflalo <soundswitch+github@aaflalo.me>
2021-08-24 10:25:00 -0400

    refactor(AudioDeviceBox): Use proper payload

--- SoundSwitch/UI/Forms/Components/AudioDeviceBox.cs
+++ SoundSwitch/UI/Forms/Components/AudioDeviceBox.cs
@@ -1,14 +1,41 @@
-﻿using System.Windows.Forms;
-using SoundSwitch.Common.Framework.Audio.Device;
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace SoundSwitch.UI.Forms.Components
 {
     public partial class AudioDeviceBox : UserControl
     {
-        public AudioDeviceBox(DeviceFullInfo device)
+        private bool _selected;
+
+        public record Payload(Image Image, string Label, bool Selected);
+
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        [DefaultValue(false)]
+        public bool Selected
         {
-            iconBox.DataBindings.Add(nameof(PictureBox.Image), device, nameof(device.LargeIcon), false, DataSourceUpdateMode.OnPropertyChanged);
-            deviceName.DataBindings.Add(nameof(Label.Text), device, nameof(device.NameClean), false, DataSourceUpdateMode.OnPropertyChanged);
+            get => _selected;
+            set
+            {
+                var oldValue = _selected;
+                _selected = value;
+                OnSelectedChanged(oldValue, _selected);
+            }
+        }
+
+
+        public AudioDeviceBox(Payload payload)
+        {
+            iconBox.DataBindings.Add(nameof(PictureBox.Image), payload, nameof(payload.Image), false, DataSourceUpdateMode.OnPropertyChanged);
+            deviceName.DataBindings.Add(nameof(Label.Text), payload, nameof(payload.Label), false, DataSourceUpdateMode.OnPropertyChanged);
+            Selected = payload.Selected;
+            
+            InitializeComponent();
+        }
+
+        protected void OnSelectedChanged(bool oldValue, bool newValue)
+        {
+            ForeColor = newValue ? Color.Teal : Color.Black;
         }
     }
 }
