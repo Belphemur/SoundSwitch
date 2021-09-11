89a9476188e9f923202147add449fa5aa81e077a
Antoine Aflalo <soundswitch+github@aaflalo.me>
2021-09-11 13:20:27 -0400

    ci(DevRelease): Add proper changelog

---
+++ devRelease.js
@@ -0,0 +1,35 @@
+'use strict'
+const util = require('util');
+const {Webhook, MessageBuilder} = require("discord-webhook-node");
+const exec = util.promisify(require('child_process').exec);
+const webhookUrl = process.argv[2];
+const version = process.argv[3];
+const commitRegex = /^([\d\w]{7})\s(\w+)\(([\w-]+)\)(.+)/gm;
+const commitSubstitution = "* **$2**(*$3*)$4 ($1)[https://github.com/Belphemur/SoundSwitch/commit/$1]";
+
+async function main() {
+    const hook = new Webhook(webhookUrl);
+    const embed = new MessageBuilder()
+        .setAuthor("Beta Build", "https://soundswitch.aaflalo.me/img/beta.png")
+        .setTitle(`New Build: ${version}`)
+        .setURL("https://nc.aaflalo.me/s/8DKytYCTAbrn6yz")
+        .setColor("#0000FF");
+
+    try {
+        let result = await exec("git describe --tags --abbrev=0");
+        const describe = result.stdout.replace("\n", "");
+        result = await exec(`git log  --no-merges --oneline ${describe}..HEAD`);
+
+        const commits = result.stdout.replace(commitRegex, commitSubstitution);
+        const description = `**Since ${describe}**\n\n${commits}`;
+        embed.setDescription(description)
+            .setTimestamp();
+        console.log(description);
+        hook.send(embed);
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+main()
+
