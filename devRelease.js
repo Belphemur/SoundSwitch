1e21d881307bd6924ac9d76e11ca367474687201
Antoine Aflalo <git@aaflalo.me>
2021-09-20 16:26:55 -0400

    ci(DevRelease): Use backblaze to store nightly

--- devRelease.js
+++ devRelease.js
@@ -1,16 +1,11 @@
 'use strict'
-import Upload from '@uploadcare/upload-client'
-
-const UploadClient = Upload.default;
 import * as fs from 'fs/promises';
 import * as util from 'util';
 import * as crypto from 'crypto';
 import * as path from 'path';
-
 import {exec} from 'child_process';
-
 import {Webhook, MessageBuilder} from "discord-webhook-node";
-
+import Bucket from "backblaze";
 
 const execAsync = util.promisify(exec);
 const webhookUrl = process.argv[2];
@@ -22,39 +17,29 @@ const commitRegex = /^([\d\w]{7})\s(\w+)\((.+)\)(.+)/gm;
 const commitSubstitution = `* **$2**(*$3*)$4 [$1](https://github.com/${repo}/commit/$1)`;
 const uploadSecret = process.env.UPLOAD_SECRET;
 const uploadPublic = process.env.UPLOAD_PUBLIC;
-
-
-function generateSignature(secret, expire) {
-    const hmac = crypto.createHmac('sha256', secret);
-    hmac.update(expire);
-    return hmac.digest('hex');
-}
+const bucketName = process.env.UPLOAD_BUCKET;
 
 async function main() {
 
-
-    const file = await fs.readFile(filePath);
     try {
-        const client = new UploadClient({publicKey: uploadPublic});
 
-        const expiry = (Math.round(Date.now() / 1000) + 120).toString();
-        const signature = generateSignature(uploadSecret, expiry);
-
-        const response = await client.uploadFile(file, {
-            secureSignature: signature,
-            secureExpire: expiry,
-            fileName: path.basename(filePath)
+        const bucket = Bucket(bucketName, {
+            id: uploadPublic,
+            key: uploadSecret
         });
 
+        const fileBasename = path.basename(filePath);
+
+        const response = await bucket.upload(filePath, path.join('nightly',fileBasename));
         const hook = new Webhook(webhookUrl);
         const embed = new MessageBuilder()
             .setAuthor("Beta Build", "https://soundswitch.aaflalo.me/img/beta.png")
             .setTitle(`New Build: ${version}`)
-            .setURL(response.cdnUrl)
+            .setURL(response.url)
             .setColor("#0000FF");
 
 
-        const result = await execAsync(`git log --no-merges --oneline -10`);
+        const result = await execAsync('git log --no-merges --oneline -10');
 
         const commits = result.stdout.replace(commitRegex, commitSubstitution);
         const description = `**Last 10 commits**\n\n${commits}`;
