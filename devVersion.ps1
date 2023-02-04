afbb15429674c97c4cefd10043596f1658db89b3
Antoine Aflalo <soundswitch+github@aaflalo.me>
2023-02-04 18:39:31 -0500

    ci(DevVersion): Use new way to set the output

--- devVersion.ps1
+++ devVersion.ps1
@@ -9,5 +9,6 @@ $newRevision = [int]$currentDate.DayOfYear * 24 + $currentDate.Day + $currentDat
 $newVersion = [Version]::new($currentVersion.Major, $currentVersion.Minor, $currentVersion.Build, $newRevision)
 (Get-Content SoundSwitch/Properties/AssemblyInfo.cs) -replace "AssemblyVersion\(.+\)","AssemblyVersion(`"$newVersion`")" |  Out-File SoundSwitch/Properties/AssemblyInfo.cs
 
+"version=$($newVersion.toString())" |  Out-File $Env:GITHUB_OUTPUT
 
 echo "::set-output name=version::$($newVersion.toString())"
