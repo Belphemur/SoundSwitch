68180bf2581558f060465a20beb87a54afd087aa
Antoine Aflalo <soundswitch+github@aaflalo.me>
2023-02-04 17:33:35 -0500

    ci(Nightly): Improve nightly version generation

--- devVersion.ps1
+++ devVersion.ps1
@@ -1,13 +1,12 @@
 $latestTag = git describe --tags --abbrev=0
 $currentVersion = [Version]$latestTag.substring(1)
-$currentBuild = $Env:GITHUB_RUN_ID
 $scriptPath = $MyInvocation.MyCommand.Path
 $scriptFolder = Split-Path $scriptPath -Parent
 
-$newBuild = [int]$currentBuild.substring(0,5)
-$newRevision = [int]$currentBuild.substring($currentBuild.Length - 4)
+$currentDate = (Get-Date)
+$newRevision = [int]$currentDate.DayOfYear * 24 + $currentDate.Day + $currentDate.Hour + $currentDate.Minute
 
-$newVersion = [Version]::new($currentVersion.Major, $currentVersion.Minor, $newBuild, $newRevision)
+$newVersion = [Version]::new($currentVersion.Major, $currentVersion.Minor, $currentVersion.Build, $newRevision)
 (Get-Content SoundSwitch/Properties/AssemblyInfo.cs) -replace "AssemblyVersion\(.+\)","AssemblyVersion(`"$newVersion`")" |  Out-File SoundSwitch/Properties/AssemblyInfo.cs
 
 
