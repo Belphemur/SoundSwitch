ef1e7705da49f3ad8f872d438741db80d5581282
Antoine Aflalo <soundswitch+github@aaflalo.me>
2023-02-05 12:39:13 -0500

    Revert "ci(output): Change the way to give an output to CI steps"
    This reverts commit 679c01c48e413879876c3405669afd2209945340.

--- devVersion.ps1
+++ devVersion.ps1
@@ -9,4 +9,6 @@ $newRevision = [int]$currentDate.DayOfYear * 24 + $currentDate.Day + $currentDat
 $newVersion = [Version]::new($currentVersion.Major, $currentVersion.Minor, $currentVersion.Build, $newRevision)
 (Get-Content SoundSwitch/Properties/AssemblyInfo.cs) -replace "AssemblyVersion\(.+\)","AssemblyVersion(`"$newVersion`")" |  Out-File SoundSwitch/Properties/AssemblyInfo.cs
 
-"devVersion=$($newVersion.toString())" |  Out-File $Env:GITHUB_OUTPUT
+"version=$($newVersion.toString())" |  Out-File $Env:GITHUB_OUTPUT
+
+echo "::set-output name=version::$($newVersion.toString())"
