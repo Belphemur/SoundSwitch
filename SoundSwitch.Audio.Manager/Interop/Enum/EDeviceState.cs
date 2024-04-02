0d319688cbf27dbd0af4c1266503c16855ecc451
Antoine Aflalo <soundswitch+github@aaflalo.me>
2024-04-02 10:00:42 -0400

    feat: make audioswitcher able to provide list of devices

---
+++ SoundSwitch.Audio.Manager/Interop/Enum/EDeviceState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SoundSwitch.Audio.Manager.Interop.Enum;
+
+/// <summary>Device State</summary>
+[Flags]
+public enum EDeviceState
+{
+    /// <summary>DEVICE_STATE_ACTIVE</summary>
+    Active = 1,
+    /// <summary>DEVICE_STATE_DISABLED</summary>
+    Disabled = 2,
+    /// <summary>DEVICE_STATE_NOTPRESENT</summary>
+    NotPresent = 4,
+    /// <summary>DEVICE_STATE_UNPLUGGED</summary>
+    Unplugged = 8,
+    /// <summary>DEVICE_STATEMASK_ALL</summary>
+    All = Unplugged | NotPresent | Disabled | Active, // 0x0000000F
+}
