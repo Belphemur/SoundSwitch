4851b438c0da0aa9c0ac34ceae1ac803ad5ed4bd
Antoine Aflalo <soundswitch+github@aaflalo.me>
2024-04-02 19:29:30 -0400

    fix: comparing devices event, needs to also compare the id

--- SoundSwitch/Model/DeviceChangedEvent.cs
+++ SoundSwitch/Model/DeviceChangedEvent.cs
@@ -15,6 +15,10 @@ public record DeviceChangedEvent(EventType Action, string DeviceId) : IComparabl
 {
     public int CompareTo(DeviceChangedEvent other)
     {
-        return Action.CompareTo(other.Action);
+        if (ReferenceEquals(this, other)) return 0;
+        if (ReferenceEquals(null, other)) return 1;
+        var actionComparison = Action.CompareTo(other.Action);
+        if (actionComparison != 0) return actionComparison;
+        return string.Compare(DeviceId, other.DeviceId, StringComparison.Ordinal);
     }
-};
+}
