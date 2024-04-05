15fddc803cbed59b8dfd9f48932e99f81b2ad08f
Antoine Aflalo <soundswitch+github@aaflalo.me>
2024-04-04 21:24:27 -0400

    perf(banner): Improve how the banner is shown to the user
    Fixes SOUNDSWITCH-1PH

--- SoundSwitch/Framework/Banner/BannerManager.cs
+++ SoundSwitch/Framework/Banner/BannerManager.cs
@@ -1,16 +1,16 @@
 ﻿/********************************************************************
-* Copyright (C) 2015-2017 Antoine Aflalo
-*
-* This program is free software; you can redistribute it and/or
-* modify it under the terms of the GNU General Public License
-* as published by the Free Software Foundation; either version 2
-* of the License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-********************************************************************/
+ * Copyright (C) 2015-2017 Antoine Aflalo
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ ********************************************************************/
 
 using System;
 using Serilog;
@@ -32,7 +32,7 @@ namespace SoundSwitch.Framework.Banner
         public void ShowNotification(BannerData data)
         {
             // Execute the banner in the context of the UI thread
-            syncContext.Post((d) =>
+            syncContext.Send(_ =>
             {
                 if (banner == null)
                 {
