a827eca87ec262fa161bc4c4c341d7bdc7031ce4
Antoine Aflalo <197810+Belphemur@users.noreply.github.com>
2024-06-01 15:53:32 -0400

    test(beta): test beta version always smaller than released one

---
+++ SoundSwitch.Tests/VersionTest.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using NuGet.Versioning;
+using NUnit.Framework;
+
+namespace SoundSwitch.Tests;
+[TestFixture]
+public class VersionTest
+{
+    [Test]
+    public void TestSemanticVersionBetaSmallerThanRelease()
+    {
+        var beta = SemanticVersion.Parse("1.0.0-beta.1");
+        var release = SemanticVersion.Parse("1.0.0");
+        beta.Should().BeLessThan(release);
+    }
+}
