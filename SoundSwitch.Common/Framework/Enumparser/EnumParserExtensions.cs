118681a9a015e99f1580c6691641c8cecac0c2e5
Antoine Aflalo <197810+Belphemur@users.noreply.github.com>
2025-02-10 13:17:39 -0500

    boost(ipc): Be sure two instance of SoundSwitch can communicate.

---
+++ SoundSwitch.Common/Framework/Enumparser/EnumParserExtensions.cs
@@ -0,0 +1,20 @@
+﻿#nullable enable
+using System;
+
+namespace SoundSwitch.Common.Framework.EnumParser;
+
+public static class EnumParserExtensions
+{
+    public static T? TryParseEnum<T>(this string? value) where T : struct, Enum
+    {
+        if (value == null)
+            return null;
+
+        if (Enum.TryParse(value, out T result))
+        {
+            return result;
+        }
+
+        return null;
+    }
+}
---
+++ SoundSwitch.Common/Framework/Pipe/NamedPipe.cs
@@ -0,0 +1,46 @@
+﻿#nullable enable
+using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading;
+using System.Threading.Tasks;
+using SoundSwitch.Common.Framework.EnumParser;
+
+namespace SoundSwitch.Common.Framework.Pipe;
+
+public static class NamedPipe
+{
+    public static event Action<MessageEnum>? OnMessageReceived;
+
+    public enum MessageEnum
+    {
+        OpenSettings
+    }
+
+    public static void StartListening(string pipeName, CancellationToken token)
+    {
+        Task.Run(async () =>
+        {
+            while (!token.IsCancellationRequested)
+            {
+                await using var server = new NamedPipeServerStream(pipeName, PipeDirection.In);
+                await server.WaitForConnectionAsync(token);
+                using var reader = new StreamReader(server);
+                var message = (await reader.ReadLineAsync(token)).TryParseEnum<MessageEnum>();
+                if (message != null)
+                {
+                    OnMessageReceived?.Invoke(message.Value);
+                }
+            }
+        }, token);
+    }
+    
+    
+    public static void SendMessageToExistingInstance(string pipeName, MessageEnum messageEnum)
+    {
+        using var client = new NamedPipeClientStream(".", pipeName, PipeDirection.Out);
+        client.Connect();
+        using var writer = new StreamWriter(client);
+        writer.WriteLine(messageEnum.ToString());
+    }
+}
