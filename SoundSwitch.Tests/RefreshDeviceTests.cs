515c0c88a5db1fd94f733e0042684fb4c604ca85
Antoine Aflalo <197810+Belphemur@users.noreply.github.com>
2026-08-30 06:09:03 +0000

    fix(test): add missing System.Threading using in RefreshDeviceTests (#2404)
    Resolves CS0246 (CancellationToken not found) that broke the Windows CI build / build job.

--- SoundSwitch.Tests/RefreshDeviceTests.cs
+++ SoundSwitch.Tests/RefreshDeviceTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 
 using FluentAssertions;
